ecf2e26fe23105af45dc554eee855a9223e9992e
Oskar <o.wojcicky06@gmail.com>
2026-06-13 21:03:28 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "Chest.h"
 #include "Ptaszek.h"
 #include "Inventory.h"
+#include <SFML/Audio.hpp>
 
 enum class GameState
 {
@@ -298,10 +299,10 @@ const char (*worldMap[10][10])[15]=
     {nullptr, nullptr, room11, room10, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr}
 };
 
-void Rooms(int wx, int wy, std::vector<Game*>& wordlObjects, float startX, float startY, const sf::Texture& t_drzewa, const sf::Texture& t_floor1, const sf::Texture& t_floor2, const sf::Texture& t_cien)
+void Rooms(int wx, int wy, std::vector<Game*>& wordlObjects, float startX, float startY, const sf::Texture& t_drzewa, const sf::Texture& t_floor1, const sf::Texture& t_floor2, const sf::Texture& t_cien,const sf::Texture& t_slime, const sf::Texture& t_moblin_up,const sf::Texture& t_moblin_down,const sf::Texture& t_moblin_left,const sf::Texture& t_moblin_right)
 {
     // 1. ZAPAMIĘTYWANIE HP: Jeśli gracz już istniał, pobieramy jego obecne punkty życia
-    int currentHP = 10; // Domyślnie 10 (np. przy pierwszym uruchomieniu gry)
+    int currentHP = 3; // Domyślnie 10 (np. przy pierwszym uruchomieniu gry)
     if (player != nullptr)
     {
         currentHP = player->getHP();
@@ -361,8 +362,8 @@ void Rooms(int wx, int wy, std::vector<Game*>& wordlObjects, float startX, float
     // --- Reszta Twojego kodu z potworami (Slime, Chest, Moblin itd.) ---
     if(selectedRoom==room2)
     {
-        wordlObjects.push_back(new Slime(600.0f, 400.0f));
-        wordlObjects.push_back(new Slime(600.0f, 200.0f));
+        wordlObjects.push_back(new Slime(t_slime, 600.0f, 400.0f));
+        wordlObjects.push_back(new Slime(t_slime, 600.0f, 200.0f));
     }
     if(selectedRoom==room5)
     {
@@ -370,20 +371,20 @@ void Rooms(int wx, int wy, std::vector<Game*>& wordlObjects, float startX, float
     }
     if(selectedRoom==room3)
     {
-        wordlObjects.push_back(new Slime(600.0f, 400.0f));
-        wordlObjects.push_back(new Slime(600.0f, 200.0f));
-        wordlObjects.push_back(new Slime(300.0f, 200.0f));
+        wordlObjects.push_back(new Slime(t_slime,600.0f, 400.0f));
+        wordlObjects.push_back(new Slime(t_slime,600.0f, 200.0f));
+        wordlObjects.push_back(new Slime(t_slime,300.0f, 200.0f));
     }
     if(selectedRoom==room7)
     {
-        wordlObjects.push_back(new Moblin(100.0f, 100.0f));
+        wordlObjects.push_back(new Moblin(t_moblin_up,t_moblin_down,t_moblin_left,t_moblin_right,100.0f, 100.0f));
         wordlObjects.push_back(new Skieleton(400.0f, 300.0f));
     }
     if(selectedRoom==room8)
     {
-        wordlObjects.push_back(new Moblin(100.0f, 400.0f));
-        wordlObjects.push_back(new Moblin(100.0f, 300.0f));
-        wordlObjects.push_back(new Moblin(100.0f, 200.0f));
+        wordlObjects.push_back(new Moblin(t_moblin_up,t_moblin_down,t_moblin_left,t_moblin_right,100.0f, 400.0f));
+        wordlObjects.push_back(new Moblin(t_moblin_up,t_moblin_down,t_moblin_left,t_moblin_right,100.0f, 300.0f));
+        wordlObjects.push_back(new Moblin(t_moblin_up,t_moblin_down,t_moblin_left,t_moblin_right,100.0f, 200.0f));
     }
     if(selectedRoom==room9)
     {
@@ -395,14 +396,30 @@ void Rooms(int wx, int wy, std::vector<Game*>& wordlObjects, float startX, float
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(720, 528), "The legend of Zelda");
+
+    sf::Music backgroundMusic;
+    if(!backgroundMusic.openFromFile("muzyka/majoras_mask.mp3"))
+    {
+        std::cout<<"Bład w ladowaniu muzyki"<<std::endl;
+    }
+    backgroundMusic.setLoop(true);
+    backgroundMusic.setVolume(100.0f);
+    backgroundMusic.play();
+
+
     window.setFramerateLimit(60);
 
-    sf::Texture tex_drzewa, tex_floor1, tex_floor2, tex_cien, tex_serduszko;
+    sf::Texture tex_drzewa, tex_floor1, tex_floor2, tex_cien, tex_serduszko, tex_slime,tex_moblin_up,tex_moblin_down,tex_moblin_left,tex_moblin_right;
     if (!tex_drzewa.loadFromFile("grafiki/drzewa.png") ||
         !tex_floor1.loadFromFile("grafiki/floor1.png") ||
         !tex_floor2.loadFromFile("grafiki/floor2.png") ||
         !tex_cien.loadFromFile("grafiki/cien.png") ||
-        !tex_serduszko.loadFromFile("grafiki/Serduszko2.png"))
+        !tex_serduszko.loadFromFile("grafiki/Serduszko2.png") ||
+        !tex_slime.loadFromFile("grafiki/slime.png") ||
+        !tex_moblin_up.loadFromFile("grafiki/moblin_up.png")||
+        !tex_moblin_down.loadFromFile("grafiki/moblin_down.png") ||
+        !tex_moblin_left.loadFromFile("grafiki/moblin_left.png") ||
+        !tex_moblin_right.loadFromFile("grafiki/moblin_right.png"))
     {
         std::cout << "!!! Blad w ladowaniu tekstur otoczenia !!!" << std::endl;
     }
@@ -477,7 +494,7 @@ int main()
                     worldX = 1;
                     worldY = 4;
 
-                    Rooms(worldX, worldY, worldObjects, 340.0f, 240.0f, tex_drzewa,tex_floor1,tex_floor2,tex_cien);
+                    Rooms(worldX, worldY, worldObjects, 340.0f, 240.0f, tex_drzewa,tex_floor1,tex_floor2,tex_cien,tex_slime,tex_moblin_up,tex_moblin_down,tex_moblin_left,tex_moblin_right);
                     currentState = GameState::Gameplay;
                 }
             }
@@ -494,7 +511,7 @@ int main()
         
                     // Ładujemy pokój startowy na nowo. Rooms automatycznie stworzy nowego Linka.
                     // Podajemy domyślne współrzędne startowe (np. środek ekranu 340, 240)
-                    Rooms(worldX, worldY, worldObjects, 340.0f, 240.0f, tex_drzewa, tex_floor1, tex_floor2, tex_cien);
+                    Rooms(worldX, worldY, worldObjects, 340.0f, 240.0f, tex_drzewa, tex_floor1, tex_floor2, tex_cien, tex_slime,tex_moblin_up,tex_moblin_down,tex_moblin_left,tex_moblin_right);
         
                     // Ponieważ Rooms tworzy nowego Linka z 10 HP, upewniamy się, że ma pełne zdrowie
                     if (player != nullptr) {
@@ -611,7 +628,7 @@ for (size_t i = 0; i < worldObjects.size(); ++i)
                 if(worldX + 1 < 10 && worldMap[worldY][worldX + 1] != nullptr)
                 {
                     worldX++;
-                    Rooms(worldX, worldY, worldObjects, 20.0f, playerPos.y, tex_drzewa,tex_floor1,tex_floor2,tex_cien);
+                    Rooms(worldX, worldY, worldObjects, 20.0f, playerPos.y, tex_drzewa,tex_floor1,tex_floor2,tex_cien,tex_slime,tex_moblin_up,tex_moblin_down,tex_moblin_left,tex_moblin_right);
                 }
                 else
                 {
@@ -624,7 +641,7 @@ for (size_t i = 0; i < worldObjects.size(); ++i)
                 if(worldX - 1 >= 0 && worldMap[worldY][worldX - 1] != nullptr)
                 {
                     worldX--;
-                    Rooms(worldX, worldY, worldObjects, 630.0f, playerPos.y, tex_drzewa,tex_floor1,tex_floor2,tex_cien);
+                    Rooms(worldX, worldY, worldObjects, 630.0f, playerPos.y, tex_drzewa,tex_floor1,tex_floor2,tex_cien,tex_slime,tex_moblin_up,tex_moblin_down,tex_moblin_left,tex_moblin_right);
                 }
                 else
                 {
@@ -636,7 +653,7 @@ for (size_t i = 0; i < worldObjects.size(); ++i)
                 if(worldY + 1 < 10 && worldMap[worldY + 1][worldX] != nullptr)
                 {
                     worldY++;
-                    Rooms(worldX, worldY, worldObjects, playerPos.x, 20.0f, tex_drzewa,tex_floor1,tex_floor2,tex_cien);
+                    Rooms(worldX, worldY, worldObjects, playerPos.x, 20.0f, tex_drzewa,tex_floor1,tex_floor2,tex_cien,tex_slime,tex_moblin_up,tex_moblin_down,tex_moblin_left,tex_moblin_right);
                 }
                 else
                 {
@@ -648,7 +665,7 @@ for (size_t i = 0; i < worldObjects.size(); ++i)
                 if(worldY - 1 >= 0 && worldMap[worldY - 1][worldX] != nullptr)
                 {
                     worldY--;
-                    Rooms(worldX, worldY, worldObjects, playerPos.x, 500.0f, tex_drzewa,tex_floor1,tex_floor2,tex_cien);
+                    Rooms(worldX, worldY, worldObjects, playerPos.x, 500.0f, tex_drzewa,tex_floor1,tex_floor2,tex_cien,tex_slime,tex_moblin_up,tex_moblin_down,tex_moblin_left,tex_moblin_right);
                 }
                 else 
                 {
@@ -879,7 +896,7 @@ for (size_t i = 0; i < worldObjects.size(); ++i)
                 worldX = 3;
                 worldY = 9;
 
-                Rooms(worldX,worldY,worldObjects,340.0f,240.0f, tex_drzewa,tex_floor1,tex_floor2,tex_cien);
+                Rooms(worldX,worldY,worldObjects,340.0f,240.0f, tex_drzewa,tex_floor1,tex_floor2,tex_cien,tex_slime,tex_moblin_up,tex_moblin_down,tex_moblin_left,tex_moblin_right);
 
                 currentState = GameState::Gameplay;
             }
@@ -944,7 +961,7 @@ for (size_t i = 0; i < worldObjects.size(); ++i)
         {
             for(auto& object : worldObjects)
             {
-                object->draw(window);
+                object->draw(window); 
             }
             window.draw(gameOverCurtain);
 
